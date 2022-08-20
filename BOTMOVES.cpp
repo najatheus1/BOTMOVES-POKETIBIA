12886e5dd546688fe01cba0d5de1f327170aa568
najatheus1 <72266626+najatheus1@users.noreply.github.com>
2022-08-20 15:03:50 -0300

    Update BOTMOVES.cpp

--- BOTMOVES.cpp
+++ BOTMOVES.cpp
@@ -1,34 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <Windows.h>
-#include <string>
 #include <iostream>
-
-std::string getLocalTime() {
-	time_t time_;
-
-	std::time(&time_);
-	tm* s = localtime(&time_);
-
-	std::string time__ = std::to_string(s->tm_mday) + "/" + std::to_string(s->tm_mon + 1) +
-		"/" + std::to_string(s->tm_year + 1900) + " [Horas:" + std::to_string(s->tm_hour) +
-		" Minutos: " + std::to_string(s->tm_min) + " Segundo: " + std::to_string(s->tm_sec) +
-		+"]";
-
-	return time__;
-}
 
 int main()
 {
 	SetConsoleTitle("BOT MOVES (BY : SRNAJA)");
 
-	char key;
+	char key, key2;
 	char* name = new char[50];
 	std::cout << "DIGITE O NOME DA JANELA DO POKETIBIA : " << std::endl;
 	std::cin >> name;
 	std::system("cls");
-	std::cout << "DIGITE A KEY:" << std::endl;
+	std::cout << "DIGITE A KEY QUE VOCE DESEJA LIGAR O BOT:" << std::endl;
 	std::cin >> key;
+	std::system("cls");
+	std::cout << "DIGITE A KEY QUE VOCE DESEJA DESLIGAR O BOT:" << std::endl;
+	std::cin >> key2;
 	std::system("cls");
 	HWND window = 0x00, window_ = 0x00;
 
@@ -37,6 +23,20 @@ int main()
 		if (window) {
 			window_ = GetForegroundWindow();
 			if (window == window_) {
+				if (GetAsyncKeyState(toupper(key2))) {
+					keybd_event(VK_F1, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F2, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F3, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F4, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F5, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F6, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F7, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F8, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F9, NULL, KEYEVENTF_KEYUP, NULL);
+					keybd_event(VK_F10, NULL, KEYEVENTF_KEYUP, NULL);
+					std::system("cls");
+					std::cout << "BOT PARADO!" << std::endl;
+				}
 				if (GetAsyncKeyState(toupper(key))) {
 					keybd_event(VK_F1, NULL, NULL, NULL);
 					keybd_event(VK_F2, NULL, NULL, NULL);
@@ -48,18 +48,13 @@ int main()
 					keybd_event(VK_F8, NULL, NULL, NULL);
 					keybd_event(VK_F9, NULL, NULL, NULL);
 					keybd_event(VK_F10, NULL, NULL, NULL);
-					keybd_event(VK_F1, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F2, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F3, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F4, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F5, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F6, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F7, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F8, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F9, NULL, KEYEVENTF_KEYUP, NULL);
-					keybd_event(VK_F10, NULL, KEYEVENTF_KEYUP, NULL);
-					std::cout << getLocalTime() << "ATAQUE CONCLUIDO" << std::endl;
+					std::system("cls");
+					std::cout << "BOT iniciado!" << std::endl;
 				}
+			}
+			else {
+				std::system("cls");
+				std::cout << "BOT PARADO!" << std::endl;
 			}
 		}
 	}
